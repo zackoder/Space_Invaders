22168ee903da980f921e73ed20d397e5e8658f2f
zbessado <bessadouz@gmail.com>
2025-01-20 20:47:51 +0100

    hello

--- index.js
+++ index.js
@@ -5,32 +5,33 @@ const lay_out_GameOver = document.querySelector(".lay_out_GameOver");
 const lay_out_YouWin = document.querySelector(".lay_out_YouWin");
 const livesspan = document.querySelector(".livesspan");
 const levelspan = document.querySelector(".levelspan");
-
-let lives = 3;
-let level = 0;
-let win = false;
-let topp = 30;
-let left = 0;
-let enemies = [];
-let bullets = [];
-let score = 0;
-let bestScore = localStorage.getItem("bestScore") || 0;
-let isPaused = true;
-let isGameOver = false;
-let enemesPositions = [];
-
-let playerPosition = 255;
-let keysPressed = {};
-let enemyDirection = 1;
-const enemySpeed = 2;
-const bulletSpeed = 5;
-const playerSpeed = 3;
-
-let gamewidth = game.clientWidth - 50;
-
 const scoreDisplay = document.querySelector("#score");
 const bestScoreDisplay = document.querySelector(".bestscore");
 const currentScoreDisplay = document.querySelector(".score");
+const alienscontainer = document.querySelector(".alienscontainer");
+
+let lives = 3;
+let level = 0;
+let topp = 0;
+let left = 0;
+let score = 0;
+let playerPosition = 255;
+let enemyDirection = 1;
+let alienscontainerheigth = 0;
+const enemySpeed = 2;
+const bulletSpeed = 5;
+const playerSpeed = 3;
+let gamewidth = game.clientWidth;
+let bestScore = localStorage.getItem("bestScore") || 0;
+let win = false;
+let isPaused = true;
+let isGameOver = false;
+let enemies = [];
+let bullets = [];
+let enemesPositions = [];
+let keysPressed = {};
+let aliensContainerPosition = { Left: 0, Top: 35 };
+
 bestScoreDisplay.innerHTML = `Best Score: ${bestScore}`;
 currentScoreDisplay.innerHTML = `Score: ${score}`;
 
@@ -46,6 +47,8 @@ livesspan.innerHTML = lives;
 levelspan.innerHTML = level;
 
 function drawEnemies() {
+  aliensContainerPosition = { Left: 0, Top: 35 };
+
   for (let i = 0; i < 3 + level; i++) {
     for (let j = 0; j < 7; j++) {
       const alien = new Image();
@@ -57,12 +60,13 @@ function drawEnemies() {
       let enemyPosition = { Left: left, Top: topp };
       enemesPositions.push(enemyPosition);
       left += 55;
-      game.appendChild(alien);
+      alienscontainer.appendChild(alien);
       enemies.push(alien);
     }
     left = 0;
     topp += 35;
   }
+  alienscontainer.style.transform = "translate(0px, 35px);";
 }
 
 drawEnemies();
@@ -71,30 +75,21 @@ let isMovingDown = false;
 
 function updateEnemies() {
   if (isGameOver || isPaused || isMovingDown) return;
+  let currentLeft = aliensContainerPosition.Left;
+  let currentTop = aliensContainerPosition.Top;
+  let newLeft = currentLeft + enemySpeed * enemyDirection;
 
-  enemies.forEach((enemy, i) => {
-    let currentLeft = enemesPositions[i].Left;
-    let currentTop = enemesPositions[i].Top;
-    let newLeft = currentLeft + enemySpeed * enemyDirection;
+  aliensContainerPosition.Left = newLeft;
+  alienscontainer.style.transform = `translate(${newLeft}px, ${currentTop}px)`;
 
-    if (newLeft <= 0 || newLeft >= gamewidth) {
-      enemyDirection *= -1;
-      isMovingDown = true;
-      moveEnemiesDownSmoothly(20, () => {
-        isMovingDown = false;
-      });
-      return;
-    }
-
-    enemesPositions[i].Left = newLeft;
-    enemy.style.transform = `translate(${newLeft}px, ${currentTop}px)`;
-
-    if (currentTop >= 520) {
-      if (!isGameOver) {
-        Gameover(lay_out_GameOver);
-      }
-    }
-  });
+  if (newLeft <= 0 || newLeft + alienscontainer.clientWidth >= gamewidth) {
+    isMovingDown = true;
+    moveEnemiesDownSmoothly(20, () => {
+      isMovingDown = false;
+    });
+    enemyDirection *= -1;
+    return;
+  }
   enemiesAnimationId = requestAnimationFrame(updateEnemies);
 }
 
@@ -107,21 +102,28 @@ function moveEnemiesDownSmoothly(steps, callback) {
       return;
     }
 
-    enemies.forEach((enemy, i) => {
-      enemesPositions[i].Top += 1;
-      enemy.style.transform = `translate(${enemesPositions[i].Left}px, ${enemesPositions[i].Top}px)`;
-    });
+    aliensContainerPosition.Top += 1;
+    alienscontainer.style.transform = `translate(${aliensContainerPosition.Left}px, ${aliensContainerPosition.Top}px)`;
 
     step++;
     requestAnimationFrame(moveStep);
   }
   moveStep();
+  enemies.forEach((enemy) => {
+    let enemyrct = enemy.getBoundingClientRect();
+    if (enemyrct.y >= 735) {
+      if (!isGameOver) {
+        Gameover(lay_out_GameOver);
+      }
+    }
+  });
 }
+
 let timer = 0;
 let timerspan = document.querySelector(".timer");
 timerspan.innerHTML = timer;
+
 function updateBullets() {
-  console.log();
   if (isGameOver || isPaused) return;
 
   for (let i = bullets.length - 1; i >= 0; i--) {
@@ -137,6 +139,7 @@ function updateBullets() {
       bulletPosition.splice(i, 1);
       continue;
     }
+
     for (let j = enemies.length - 1; j >= 0; j--) {
       let enemy = enemies[j];
       if (checkCollision(bullet, enemy)) {
@@ -158,11 +161,40 @@ function updateBullets() {
   bulletsAnimationId = requestAnimationFrame(updateBullets);
 }
 
+let pausedTime = 0;
+let totalElapsedTime = 0;
+let lastResumeTime = 0;
+
 function counter() {
-  if (isPaused || isGameOver) return;
-  let startTime = performance.now();
-  timerspan.innerHTML = Math.ceil(startTime / 1000);
-  counterid = requestAnimationFrame(counter);
+  if (isGameOver || win) return;
+
+  if (!isPaused) {
+    if (lastResumeTime === 0) {
+      lastResumeTime = performance.now();
+    }
+    const currentTime = performance.now();
+    totalElapsedTime += currentTime - lastResumeTime;
+    lastResumeTime = currentTime;
+    timerspan.innerHTML = Math.ceil(totalElapsedTime / 1000);
+    counterid = requestAnimationFrame(counter);
+  } else {
+    lastResumeTime = performance.now();
+  }
+}
+
+function Gameover(element) {
+  lives--;
+  livesspan.innerHTML = lives;
+  if (lives > 0) {
+    resetGame();
+  } else {
+    isGameOver = true;
+    cancelAnimations();
+    element.style.display = "flex";
+    checkScore();
+    lives = 3;
+    resetGame();
+  }
 }
 
 function nextlevel(LayOut) {
@@ -189,62 +221,27 @@ function checkScore() {
   }
 }
 
-function handlePlayerMovement() {
-  if (isGameOver || isPaused) return;
-  if (keysPressed["ArrowLeft"] && playerPosition > 0) {
-    player.style.transform = `translate(${
-      playerPosition - playerSpeed
-    }px, 550px)`;
-    playerPosition -= playerSpeed;
-  }
-  if (keysPressed["ArrowRight"] && playerPosition < 550) {
-    player.style.transform = `translate(${
-      playerPosition + playerSpeed
-    }px, 550px)`;
-    playerPosition += playerSpeed;
-  }
-  playerAnimationId = requestAnimationFrame(handlePlayerMovement);
-}
-
 function checkCollision(obj1, obj2) {
-  const rect1 = obj1.getBoundingClientRect();
-  const rect2 = obj2.getBoundingClientRect();
-
+  const bullet = obj1.getBoundingClientRect();
+  const alien = obj2.getBoundingClientRect();
   return !(
-    rect1.top > rect2.bottom ||
-    rect1.bottom < rect2.top ||
-    rect1.left > rect2.right ||
-    rect1.right < rect2.left
+    bullet.top > alien.bottom ||
+    bullet.bottom < alien.top ||
+    bullet.left > alien.right ||
+    bullet.right < alien.left
   );
-}
-
-function Gameover(element) {
-  lives--;
-  livesspan.innerHTML = lives;
-  resetGame();
-  if (lives === 0) {
-    isGameOver = true;
-    score = 0;
-    cancelAnimations();
-    element.style.display = "flex";
-    checkScore();
-    lives = 3;
-  }
 }
 
 let bulletPosition = [];
 
 function fireBullet() {
   if (isPaused) return;
-  console.log(keysPressed[" "]);
-
   if (keysPressed[" "]) {
     if (!firingCooldown) {
       const playerLeft = playerPosition + 23;
       const bullet = document.createElement("div");
       bullet.style.transform = `translate(${playerLeft}px, 550px)`;
       let bulletP = { Left: playerLeft, Top: 550 };
-
       bullet.className = "bullet";
       game.appendChild(bullet);
       bulletPosition.push(bulletP);
@@ -256,30 +253,33 @@ function fireBullet() {
 }
 
 function resetGame() {
-  topp = 20;
+  topp = 0;
   left = 0;
   win = false;
   isGameOver = false;
-  isPaused = false;
+  isPaused = true;
   enemesPositions = [];
   bulletPosition = [];
   enemyDirection = 1;
+  totalElapsedTime = 0;
+  lastResumeTime = 0;
+  timerspan.innerHTML = 0;
+  score = 0;
   currentScoreDisplay.innerHTML = `Score: ${score}`;
+  livesspan.innerHTML = lives;
+
   enemies.forEach((enemy) => enemy.remove());
   bullets.forEach((bullet) => bullet.remove());
   bullets = [];
   enemies = [];
-  bulletPosition = [];
-  drawEnemies();
-  lay_out_GameOver.style.display = "none";
-  lay_out_YouWin.style.display = "none";
-  lay_out_Pausse.style.display = "none";
 
   cancelAnimations();
 
-  enemiesAnimationId = requestAnimationFrame(updateEnemies);
-  bulletsAnimationId = requestAnimationFrame(updateBullets);
-  playerAnimationId = requestAnimationFrame(handlePlayerMovement);
+  drawEnemies();
+  alienscontainer.style.transform = `translate(0px, 35px)`;
+  lay_out_GameOver.style.display = "none";
+  lay_out_YouWin.style.display = "none";
+  lay_out_Pausse.style.display = "none";
 }
 
 function cancelAnimations() {
@@ -293,27 +293,6 @@ function cancelAnimations() {
   playerAnimationId = null;
   counterid = null;
 }
-
-document.addEventListener("keydown", (e) => {
-  keysPressed[e.key] = true;
-  fireBullet();
-
-  if (e.key.toLowerCase() === "p" && !isGameOver && !win) {
-    isPaused = !isPaused;
-    if (!isPaused) {
-      lay_out_Pausse.style.display = "none";
-
-      cancelAnimations();
-
-      enemiesAnimationId = requestAnimationFrame(updateEnemies);
-      bulletsAnimationId = requestAnimationFrame(updateBullets);
-      playerAnimationId = requestAnimationFrame(handlePlayerMovement);
-      counterid = requestAnimationFrame(counter);
-    } else {
-      lay_out_Pausse.style.display = "flex";
-    }
-  }
-});
 
 function startgame() {
   if (bestScore < score) {
@@ -329,15 +308,51 @@ function startgame() {
   enemiesAnimationId = requestAnimationFrame(updateEnemies);
   bulletsAnimationId = requestAnimationFrame(updateBullets);
   playerAnimationId = requestAnimationFrame(handlePlayerMovement);
-  counterid = requestAnimationFrame(counter);
 }
 
+function handlePlayerMovement() {
+  if (isGameOver || isPaused) return;
+
+  if (keysPressed["ArrowLeft"] && playerPosition > 0) {
+    playerPosition -= playerSpeed;
+    player.style.transform = `translate(${playerPosition}px, 550px)`;
+  }
+
+  if (keysPressed["ArrowRight"] && playerPosition < gamewidth - 50) {
+    playerPosition += playerSpeed;
+    player.style.transform = `translate(${playerPosition}px, 550px)`;
+  }
+
+  if (keysPressed[" "]) {
+    fireBullet();
+  }
+
+  playerAnimationId = requestAnimationFrame(handlePlayerMovement);
+}
+
+document.addEventListener("keydown", (e) => {
+  keysPressed[e.key] = true;
+  if (e.key.toLowerCase() === "p" && !isGameOver && !win) {
+    isPaused = !isPaused;
+    if (!isPaused) {
+      lay_out_Pausse.style.display = "none";
+      cancelAnimations();
+      enemiesAnimationId = requestAnimationFrame(updateEnemies);
+      bulletsAnimationId = requestAnimationFrame(updateBullets);
+      playerAnimationId = requestAnimationFrame(handlePlayerMovement);
+    } else {
+      lay_out_Pausse.style.display = "flex";
+    }
+  }
+});
+
 document.addEventListener("keyup", (e) => {
-  keysPressed[e.key] = false;
-  console.log("hello");
+  if (keysPressed[e.key]) {
+    keysPressed[e.key] = false;
+  }
 });
 
 enemiesAnimationId = requestAnimationFrame(updateEnemies);
 bulletsAnimationId = requestAnimationFrame(updateBullets);
 playerAnimationId = requestAnimationFrame(handlePlayerMovement);
-counterid = requestAnimationFrame(counter);
+requestAnimationFrame(counter);
